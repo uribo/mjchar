3838f5da668c14c73f411f4a8e211a986c35c5e2
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2022-06-22 08:38:30 +0900

    [feature]字辞典の情報をデフォルトで非表示

--- R/information.R
+++ R/information.R
@@ -1,13 +1,20 @@
 #' List for moji information
 #'
 #' @inheritParams moji_to_mjcode
+#' @param .keep_dictionary_page Whether to include page numbers in the
+#' various dictionaries.
 #' @examples
 #' moji_information("\U74dc")
+#' moji_information("\U74dc", .keep_dictionary_page = TRUE)
 #' @export
-moji_information <- function(x) {
-  as.list(
+moji_information <- function(x, .keep_dictionary_page = FALSE) {
+  df <-
     notfound_moji_tbl(
-      mji_full[which(mji_full[[1]] == x), ]
-    )
-  )
+      mji_full[which(mji_full[[1]] == x), ])
+  if (.keep_dictionary_page == FALSE) {
+    df <-
+      df[, c(seq_len(28),
+             seq.int(34, 35))]
+  }
+  as.list(df)
 }
